28f3410606b829966f347d7aed8758c0799bf5eb
MATSUMURA Tetsuro <matsumura.t.lab@gmail.com>
2017-01-22 13:29:38 +0900

    impl JavaScriptGenerator

---
+++ src/main/java/com/github/kmizu/nub/JavaScriptGenerator.java
@@ -0,0 +1,224 @@
+package com.github.kmizu.nub;
+
+import java.util.*;
+
+public class JavaScriptGenerator implements AstNode.ExpressionVisitor<Object> {
+
+    private Set<String> functions = new HashSet<>();
+
+    private StringBuilder stringBuilder = new StringBuilder();
+
+
+    public Object visitBinaryOperation(AstNode.BinaryOperation node) {
+        switch (node.operator()) {
+            case "+":
+            case "-":
+            case "*":
+            case "<=":
+            case ">=":
+            case "<":
+            case ">":
+            case "&&":
+            case "||":
+                stringBuilder.append('(');
+                node.lhs().accept(this);
+                stringBuilder.append(node.operator());
+                node.rhs().accept(this);
+                stringBuilder.append(')');
+                return null;
+            case "/":
+                stringBuilder.append("((");
+                node.lhs().accept(this);
+                stringBuilder.append(node.operator());
+                node.rhs().accept(this);
+                stringBuilder.append("|0)");
+                return null;
+            case "==":
+                stringBuilder.append('(');
+                node.lhs().accept(this);
+                stringBuilder.append("===");
+                node.rhs().accept(this);
+                stringBuilder.append(')');
+                return null;
+            case "!=":
+                stringBuilder.append('(');
+                node.lhs().accept(this);
+                stringBuilder.append("!==");
+                node.rhs().accept(this);
+                stringBuilder.append(')');
+                return null;
+            default:
+                throw new RuntimeException("cannot reach here");
+        }
+    }
+
+    @Override
+    public Object visitNumber(AstNode.Number node) {
+        stringBuilder.append(node.value());
+        return null;
+    }
+
+    @Override
+    public Object visitLetExpression(AstNode.LetExpression node) {
+        stringBuilder.append("let ");
+        stringBuilder.append(node.variableName());
+        stringBuilder.append(" = ");
+        Object value = node.expression().accept(this);
+        return null;
+    }
+
+    @Override
+    public Object visitAssignmentOperation(AstNode.AssignmentOperation node) {
+        stringBuilder.append("(");
+        stringBuilder.append(node.variableName());
+        stringBuilder.append(" = ");
+        Object value = node.expression().accept(this);
+        stringBuilder.append(")");
+        return null;
+    }
+
+    @Override
+    public Object visitPrintExpression(AstNode.PrintExpression node) {
+        stringBuilder.append("console.log(");
+        node.target().accept(this);
+        stringBuilder.append(");");
+        return null;
+    }
+
+    @Override
+    public Object visitPrintlnExpression(AstNode.PrintlnExpression node) {
+        stringBuilder.append("console.log(");
+        node.target().accept(this);
+        stringBuilder.append(");");
+        return null;
+    }
+
+    private void visitExpressions(List<AstNode.Expression> nodes)
+    {
+        for (AstNode.Expression e : nodes) {
+            if(e instanceof AstNode.DefFunction) {
+                e.accept(this);
+                stringBuilder.append(";");
+                stringBuilder.append("__last__ = ");
+                stringBuilder.append(((AstNode.DefFunction)e).name());
+            } else if(e instanceof AstNode.LetExpression) {
+                e.accept(this);
+                stringBuilder.append(";");
+                stringBuilder.append("__last__ = ");
+                stringBuilder.append(((AstNode.LetExpression)e).variableName());
+            } else {
+                stringBuilder.append("__last__ = ");
+                e.accept(this);
+            }
+            stringBuilder.append(";");
+        }
+    }
+
+    @Override
+    public Object visitExpressionList(AstNode.ExpressionList node) {
+        stringBuilder.append("(function(){ let __last__ = 0;"); {
+            visitExpressions(node.expressions());
+            stringBuilder.append("return __last__;})();");
+        }
+        return null;
+    }
+
+    @Override
+    public Object visitWhileExpression(AstNode.WhileExpression node) {
+        stringBuilder.append("(function(){ let __last__ = 0;");
+        {
+            stringBuilder.append("while( ");
+            node.condition().accept(this);
+            stringBuilder.append(" ){");
+            {
+                visitExpressions(node.body());
+                stringBuilder.append("}");
+            }
+            stringBuilder.append("return __last__;})();");
+        }
+        return null;
+    }
+
+    @Override
+    public Object visitIfExpression(AstNode.IfExpression node) {
+        stringBuilder.append("(function(){ let __last__ = 0;"); {
+            stringBuilder.append("if( ");
+            node.condition().accept(this);
+            stringBuilder.append(" ){");
+            {
+                visitExpressions(node.thenClause());
+                stringBuilder.append("}");
+            }
+            if(node.elseClause().size() > 0) {
+                stringBuilder.append("else{"); {
+                    visitExpressions(node.elseClause());
+                    stringBuilder.append("}");
+                }
+            }
+            stringBuilder.append("return __last__;})();");
+        }
+        return null;
+    }
+
+    @Override
+    public Object visitDefFunction(AstNode.DefFunction node) {
+        stringBuilder.append("let ");
+        stringBuilder.append(node.name());
+        stringBuilder.append(" = function(");
+        boolean isFirst = true;
+        for(String e:node.args()) {
+            stringBuilder.append(e);
+            if(isFirst){
+                isFirst = false;
+                stringBuilder.append(",");
+            }
+        }
+        stringBuilder.append("){ let __last__ = 0;"); {
+            visitExpressions(node.body());
+            stringBuilder.append("return __last__;}");
+        }
+        return null;
+    }
+
+    @Override
+    public Object visitIdentifier(AstNode.Identifier node) {
+        stringBuilder.append(node.name());
+        return null;
+    }
+
+    public Object evaluate(AstNode.ExpressionList program) {
+        stringBuilder = new StringBuilder();
+        program.accept(this);
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public Object visitFunctionCall(AstNode.FunctionCall node) {
+        //if(!functions.contains(node.name())) {
+        //    throw new NubRuntimeException("function " + node.name().name() + " is not defined");
+        //}
+        {
+            stringBuilder.append("(");
+            node.name().accept(this);
+            stringBuilder.append(")(");
+            boolean isFirst = true;
+            for(AstNode.Expression e:node.params()) {
+                e.accept(this);
+                if(isFirst){
+                    isFirst = false;
+                    stringBuilder.append(",");
+                }
+            }
+            stringBuilder.append(")");
+            return null;
+        }
+    }
+
+    @Override
+    public Object visitReturn(AstNode.Return node) {
+        stringBuilder.append("return ");
+        node.expression().accept(this);
+        stringBuilder.append(";");
+        return null;
+    }
+}
--- src/main/java/com/github/kmizu/nub/Main.java
+++ src/main/java/com/github/kmizu/nub/Main.java
@@ -10,15 +10,19 @@ public class Main {
         if(args.length == 0) {
             CommonTokenStream stream = Streams.streamFrom("def printRange(from, to) { let i = from; while(i < to) { println(i); i = i + 1; } } printRange(1, 10); // Loop");
             AstNode.ExpressionList program = new NubParser(stream).program().e;
-            Evaluator evaluator = new Evaluator();
-            evaluator.evaluate(program);
+            //Evaluator evaluator = new Evaluator();
+            //evaluator.evaluate(program);
+            JavaScriptGenerator evaluator = new JavaScriptGenerator();
+            System.out.println(evaluator.evaluate(program));
         } else {
             String fileName = args[0];
             CommonTokenStream stream = Streams.streamFrom(new File(fileName));
             NubParser parser = new NubParser(stream);
             AstNode.ExpressionList program = parser.program().e;
-            Evaluator evaluator = new Evaluator();
-            evaluator.evaluate(program);
+            //Evaluator evaluator = new Evaluator();
+            //evaluator.evaluate(program)
+            JavaScriptGenerator evaluator = new JavaScriptGenerator();
+            System.out.println(evaluator.evaluate(program));
         }
     }
 }
